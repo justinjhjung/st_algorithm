421ecfbf628f7b482d15b2b1d6ae65b27e9caa47
justinjhjung <snujustin12@gmail.com>
2018-09-25 15:16:34 +0900

    [최적화 필요] 연속하는 최장 공통 부분 문자열 길이 찾기 1단계 최적화. 2차원 테이블..

---
+++ DP/LCS_5582.py
@@ -0,0 +1,30 @@
+# Longest Common Substring (연속하는 공통 부분 문자열)
+from sys import stdin
+
+def input():
+    return stdin.readline().strip()
+
+S = str(input())
+T = str(input())
+len_s = len(S)
+len_t = len(T)
+
+dp = [[0 for j in range(len_t+1)] for i in range(2)]
+
+max_ = 0
+for i in range(len_s):
+    for j in range(len_t):
+        if S[i] == T[j]:
+            dp[1][j+1] = dp[0][j] + 1
+
+        # Difference with Longest common subsequence is that
+        # we don't do anything with the occasion where S[i] != T[i].
+        # Then dp[i+1][j+1] stays 0, which would function as
+        # "Renewal" of the substring that follows.
+
+        # Below is the renewal of max_substring length.
+        if dp[1][j+1] > max_:
+            max_ = dp[1][j+1]
+    dp[0] = dp[1].copy()
+
+print(max_)
